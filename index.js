92a238345f7f88f857a9f5ba4ede5279fb1a9cff
Lauren Shaughnessy <lrn.nessy@gmail.com>
2022-12-20 12:29:45 -0500

    commit tree f72a48677cc00b6268a5dc582826d0e25559555f

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 =31;
+let num2 = 2;
+let num3 = 39;
+let num4 = 7;
+let num5 = 20;
 
-// Write your code here
+const multiply = (num1 * num2);
+const random = (Math.ceil(Math.random()) + 1);
+const mod = (num3 % num4);
+const max = Number.parseInt ("20", 10);
